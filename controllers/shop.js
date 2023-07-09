66f16328e4eaaacfc305b26aed1be35f0748fb9f
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-09 11:35:53 +0530

    shop controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -51,20 +51,17 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
+  console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$",req.user)
   req.user
     .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
+    .then(products => {
+      console.log("get cart",products)
           res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
             products: products
           });
-        })
-        .catch(err => console.log(err));
-    })
+       })
     .catch(err => console.log(err));
 };
 
@@ -75,51 +72,16 @@ exports.postCart = (req, res, next) => {
     return req.user.addToCart(product);
   })
   .then(result=>{
-    console.log(result)
+    console.log(result);
+    res.redirect('/cart');
   })
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user
-  //   .getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } });
-  //   })
-  //   .then(products => {
-  //     let product;
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findById(prodId);
-  //   })
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity }
-  //     });
-  //   })
-  //   .then(() => {
-  //     res.redirect('/cart');
-  //   })
-  //   .catch(err => console.log(err));
+ 
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .getCart()
-    .then(cart => {
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then(products => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
+    .deleteItemFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
