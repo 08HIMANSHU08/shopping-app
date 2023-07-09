31bdf81dcbc543f3f33dcbfd34d5864007ae1f73
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-09 16:24:32 +0530

    app.js file

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+
 const errorController = require('./controllers/error');
 
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -19,20 +20,25 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('64a93a8cf4b710d36139fdf1')
-    .then(user => {
-      req.user = new User(user.name,user.email,user.cart,user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('64a93a8cf4b710d36139fdf1')
+//     .then(user => {
+//       req.user = new User(user.name,user.email,user.cart,user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(()=>{
+mongoose.connect('mongodb+srv://himanshu08:1WEPR9MY6TgzkSPM@cluster0.l2yrw6k.mongodb.net/shop?retryWrites=true&w=majority')
+.then(result=>{
+  console.log("connected")
   app.listen(3000);
+})
+.catch(err=>{
+  console.log(err);
 })
